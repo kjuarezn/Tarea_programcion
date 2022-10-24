e4f20ba075512e40b75a083fff5fbe5948dd0198
kjuarezn <70358983+kjuarezn@users.noreply.github.com>
2022-10-23 20:10:39 -0600

    .
    .

---
+++ app_bkf/Modelo/ConexionBD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Modelo
+{
+    public class ConexionBD{
+        private string cadena = "server=localhost;database=db_empresa; user=root; password=12345678";
+        public MySqlConnection conectar = new MySqlConnection();
+        public void AbrirConexion(){
+            try
+            {
+                //conectar = new MySqlConnection();
+                conectar.ConnectionString = cadena;
+                conectar.Open();
+                //System.Diagnostics.Debug.WriteLine("Conexion exitosa");
+            }
+            catch (Exception ex) {
+                //System.Diagnostics.Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public void CerrarConexion()
+        {
+            try
+            {
+                if (conectar.State == ConnectionState.Open)
+                {
+                    conectar.Close();
+                    //System.Diagnostics.Debug.WriteLine("Cerrar Conexion");
+
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
